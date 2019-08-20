030b5b7b50385e0457a70eb64718fd24ddd7607b
patrickpatso <patrickpatso95@gmail.com>
2019-08-21 04:27:40 +0800

    modified the voice.java file inside the FB folder!

--- voice.java
+++ voice.java
@@ -1,6 +1,7 @@
 class voice{
 	public static void main(String [] args){
-		System.out.println("we good now..);
+		System.out.println("we good now..");
+		System.out.println("Hello world and Android developers!");
 	}
 	
 }
